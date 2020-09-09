0bee569d645d88b931ed02cb8044d80acc70cc64
luccasdev <lucasrti@hotmail.com>
2020-09-09 00:46:16 -0300

    Adjusts in abstract class String Formatter

--- strings/JavaTemplate/src/main/java/idwall/desafio/string/IdwallFormatter.java
+++ strings/JavaTemplate/src/main/java/idwall/desafio/string/IdwallFormatter.java
@@ -1,5 +1,7 @@
 package idwall.desafio.string;
 
+import java.util.List;
+
 /**
  * Template by Rodrigo Catão Araujo on 06/02/2018.
  * Implemented by Lucas Souza on 08/09/2020.
@@ -14,6 +16,11 @@ public class IdwallFormatter extends StringFormatter {
      */
     @Override
     public String format(String text) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<String> justifyLines(List<String> lines) {
         throw new UnsupportedOperationException();
     }
 }
--- strings/JavaTemplate/src/main/java/idwall/desafio/string/StringFormatter.java
+++ strings/JavaTemplate/src/main/java/idwall/desafio/string/StringFormatter.java
@@ -1,5 +1,7 @@
 package idwall.desafio.string;
 
+import java.util.List;
+
 /**
  * Template by Rodrigo Catão Araujo on 06/02/2018.
  * Implemented by Lucas Souza on 08/09/2020.
@@ -7,16 +9,43 @@ package idwall.desafio.string;
 public abstract class StringFormatter {
 
     private Integer limit;
+    private boolean justify;
 
     public StringFormatter() {
         this.limit = 40;
+        this.justify = true;
     }
 
     /**
      * It receives a text and should return it formatted
      *
-     * @param text
-     * @return
+     * @param text - Text to apply format
+     * @return String
      */
     public abstract String format(String text);
+
+    /**
+     * It receives a text and should return it formatted
+     *
+     * @param lines - Lines to apply format
+     * @return List<String>
+     */
+    public abstract List<String> justifyLines(List<String> lines);
+
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
+
+    public boolean isJustify() {
+        return justify;
+    }
+
+    public void setJustify(boolean justify) {
+        this.justify = justify;
+    }
 }
