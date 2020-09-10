6f5f42a43bd281f60f36c1c37547b29e2441b9ae
luccasdev <lucasrti@hotmail.com>
2020-09-10 19:52:13 -0300

    Creating base of Controller and Service

---
+++ crawlers/src/main/java/luccasdev/idwall/desafio/crawler/controller/RedditThreadController.java
@@ -0,0 +1,10 @@
+package luccasdev.idwall.desafio.crawler.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/thread")
+public class RedditThreadController {
+
+}
---
+++ crawlers/src/main/java/luccasdev/idwall/desafio/crawler/service/RedditThreadService.java
@@ -0,0 +1,8 @@
+package luccasdev.idwall.desafio.crawler.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedditThreadService {
+
+}
