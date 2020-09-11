db233a6b8598ede287c1faf2c5911c54a8f72911
luccasdev <lucasrti@hotmail.com>
2020-09-10 21:02:57 -0300

    Custom Exception Handler

---
+++ crawlers/src/main/java/luccasdev/idwall/desafio/crawler/errors/CustomExceptionDto.java
@@ -0,0 +1,18 @@
+package luccasdev.idwall.desafio.crawler.errors;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class CustomExceptionDto {
+
+    private String message;
+    private int status;
+    private String error;
+    private String path;
+    private Date timestamp;
+
+}
